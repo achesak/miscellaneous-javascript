b613b9e139dfccd926aff49915e8bac27a2c8f79
achesak <achesak@yahoo.com>
2013-08-21 13:18:47 -0500

    Improved debug comments and documentation.

--- debug/debug.js
+++ debug/debug.js
@@ -1,28 +1,70 @@
-	var Debug = {
+var Debug = {
 
+// Set this value to false if debugging should be disabled.
 enabled: true,
 
+/*
+ * Debug.breakpoint(func, title)
+ * 
+ * ARGUMENTS:
+ * func - function to be run.
+ * title - optional title to use in the dialog.
+ * 
+ * RETURNS:
+ * Array of all the previous results.
+ * 
+ * NOTES:
+ * Executes the function until an empty string is entered. Shows result
+ * of last expression in the dialog, and returns all of them when the
+ * function completes.
+ */
 breakpoint: function(func, title) {
-    if(!Debug.breakpoint) {
+    
+    // If debugging is disabled, don't continue.
+    if(!Debug.enabled) {
         return false;
     }
+    
+    // Variable to remember the last expression.
     var expr = "";
+    
+    // Variable to remember the last result.
     var result;
+    
+    // Array to remember all the results.
     var resultArray = [];
+    
+    // Loop until the user says to exit.
     while (true) {
+        
+        // Build the message.
         var msg = "";
+        
+        // If a title was given, add that.
         if(title) {
             msg += title + "\n";
         }
+        
+        // Add the previous expression and result, if there was one.
         if(expr) {
             msg += expr + " -> " + result + "\n";
         }
+        
+        // Add the prompt.
         msg += "Expression:";
+        
+        // Show the prompt.
         expr = prompt(msg, expr);
+        
+        // If an empty string was entered, return the array of results.
         if(!expr) {
             return resultArray;
         }
+        
+        // Get the result.
         result = func(expr);
+        
+        // Add the result to the array.
         resultArray.push(result);
     }
 }
