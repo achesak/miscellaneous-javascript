7acb8cf7ffb922ddd782c49560d2e14d35ff0f7f
achesak <achesak@yahoo.com>
2013-08-21 13:09:30 -0500

    Improved comments for xor.

--- xor/xor.js
+++ xor/xor.js
@@ -8,9 +8,17 @@
  * XOR value of all the arguments. (True if one true value, false otherwise.)
  */
 Boolean.xor = function() {
+    
+    // Base value is false.
     var r = false;
+    
+    // Loop through every argument:
     for (var i = 0; i < arguments.length; i++) {
-       r = arguments[i] || r ? (arguments[i] && r ? false : true) : false;
+        
+        // Determine if the value should be changed.
+        r = arguments[i] || r ? (arguments[i] && r ? false : true) : false;
     }
+    
+    // Return the value.
     return r;
 };
