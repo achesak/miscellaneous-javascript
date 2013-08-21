0f7650b536166a96c0fc5dcb2bd9782ed5cafc83
achesak <achesak@yahoo.com>
2013-08-21 13:04:13 -0500

    Added documentation for timers and xor.

--- timers/timers.js
+++ timers/timers.js
@@ -1,5 +1,23 @@
 var Timers = {
 
+/*
+ * Timers.interval(func, time, obj, args)
+ * 
+ * ARGUMENTS:
+ * func - function to be executed on every interval.
+ * time - positive number specifying the interval time.
+ * obj - optional object that the function will be called as a method of. If not
+ *       specified the function is called as a method of null.
+ * args - optional array of arguments to pass the function.
+ * 
+ * RETURNS:
+ * [none]
+ * 
+ * NOTES:
+ * The return value of the function will be remembered, as passed as the first argument
+ * next time it is executed (unless the return value is undefined, in which case it will not 
+ * be passed). Returning false from the function will cancel the timer.
+ */
 interval: function(func, time, obj, args) {
     var r;
     var a = args ? args : [];
@@ -17,6 +35,19 @@ interval: function(func, time, obj, args) {
     ifunc();
 },
 
+/*
+ * Timers.timeout(func, time, obj, args)
+ * 
+ * ARGUMENTS:
+ * func - function to be executed after timeout.
+ * time - positive number specifying the timeout time.
+ * obj - optional object that the function will be called as a method of. If not
+ *       specified the function is called as a method of null.
+ * args - optional array of arguments to pass the function.
+ * 
+ * RETURNS:
+ * [none]
+ */
 timeout: function(func, time, obj, args) {
     var o = obj ? obj : null;
     if(args) {
--- xor/xor.js
+++ xor/xor.js
@@ -1,3 +1,12 @@
+/*
+ * Boolean.xor(a[, b[, c]] ...)
+ * 
+ * ARGUMENTS:
+ * any number of any type
+ * 
+ * RETURNS:
+ * XOR value of all the arguments. (True if one true value, false otherwise.)
+ */
 Boolean.xor = function() {
     var r = false;
     for (var i = 0; i < arguments.length; i++) {
